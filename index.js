1121c6da93de40e91411f08b4010885b23840659
Hitanshu Shah <hitanshushah5@gmail.com>
2022-11-05 03:34:04 +0800

    fix:Link to valid colors added in error message

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ function Coloredconsole(
     console.log(`\x1b[${Consolecolor}${message} \x1b[0m`);
   } else if (Consolecolor === undefined) {
     console.log(`\x1b[97m${message} \x1b[0m`);
-    console.log("Please provide a valid color");
+    console.log(
+      `Please provide a valid color. See list here:- \x1b[34mhttps://www.npmjs.com/package/coloredprints \x1b[0m`
+    );
   } else {
     console.log(message);
   }
